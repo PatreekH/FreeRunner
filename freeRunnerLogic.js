e018085ebbf9b3e9d6f38f5d5a15daf62b8e93e9
Patrick Hernandez <r.patrick.hernandez@hotmail.com>
2016-07-28 01:50:26 -0400

    added start ledge

--- freeRunnerLogic.js
+++ freeRunnerLogic.js
@@ -1,14 +1,9 @@
 var box = $('.box');
-var hurdle = $('.hurdle');
 var boxPos = {width: 20, height: 20}
-var hurdlePos = {width: 30, height: 30}
-
-$( document ).ready(function() {
-    $('.hurdle').animate({
-        left: '-=1800'
-    }, 3000);
-
-});
+var hurdlePos = {width: 20, height: 20}
+var i = 0;
+var z = 0;
+var k = 0;
 
 $(document).keydown(function(e) {
     switch (e.which) {
@@ -18,33 +13,49 @@ $(document).keydown(function(e) {
     }
 });
 
-var update = setInterval(function(){
-    var newHurdlePos = hurdle.position();
-    var newBoxPos = box.position();
-    if (newBoxPos.top < newHurdlePos.top + hurdlePos.width &&
-   newBoxPos.top + boxPos.width > newHurdlePos.top &&
-   newBoxPos.left < newHurdlePos.left + hurdlePos.height &&
-   boxPos.height + newBoxPos.left > newHurdlePos.left) {
-        alert("Collision!");
-    }
-}, 1);
-
 function jump(){
         $('.box').animate({
-            top: '-=75'
+            top: '-=85'
         }, 350); 
         fall()
 }
 
 function fall(){
      $('.box').animate({
-         top: '+=75'
-     }, 600);
+         top: '+=85'
+     }, 800);
 }
 
-var newHurdle = setInterval(function(){
-    $('.hurdle').css('left', '1700');
-    $('.hurdle').animate({
-        left: '-=1800'
-    }, 3000);
-}, 4000);
+/*var newHurdle = setInterval(function(){
+    createHurdle();
+}, 1500);*/
+
+//doesnt show with relative
+function createHurdle(){
+    i += 1;
+    $('.lane').append('<div class="hurdle" id="hurdle' + i + '" style="position:fixed;left:110%;top:42%;">' + '</div>');
+    moveHurdle();
+    update();
+}
+
+function moveHurdle(){
+    z += 1;
+    $('#hurdle' + z).animate({
+        left: '-=120%'
+    }, 7000);
+}
+
+function update() {
+    k += 1;
+    var hurdle = $('#hurdle' + k);
+    var update = setInterval(function(){
+        var newHurdlePos = hurdle.position();
+        var newBoxPos = box.position();
+        if (newBoxPos.top < newHurdlePos.top + hurdlePos.width &&
+       newBoxPos.top + boxPos.width > newHurdlePos.top &&
+       newBoxPos.left < newHurdlePos.left + hurdlePos.height &&
+       boxPos.height + newBoxPos.left > newHurdlePos.left) {
+            alert("Collision!");
+        }
+    }, 1);
+}
